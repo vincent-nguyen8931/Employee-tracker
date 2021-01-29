928909cff5e9bb8011a3898c6c06d79059abb12b
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-01-28 21:06:39 -0800

    Create employee table in Company_DB.

--- tracker.sql
+++ tracker.sql
@@ -1,16 +1,46 @@
-DROP DATABASE IF EXISTS showsDB;
+DROP DATABASE IF EXISTS Company_DB;
 
-CREATE DATABASE showsDB;
+CREATE DATABASE Company_DB;
 
-USE showsDB;
+USE Company_DB;
 
-CREATE TABLE cast (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE employee (
+  employeeID INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  rold_id INT(10) NOT NULL,
+  manager_id INT(10) NULL 
+  PRIMARY KEY (employeeID)
+  FOREIGN KEY (roleID) REFERENCES roles(roleID)
+  FOREIGN KEY (departmentID) REFERENCES department(departmentID)
+);
+
+CREATE TABLE roles (
+  roleID INT NOT NULL AUTO_INCREMENT,
   charName VARCHAR(45) NOT NULL,
   coolness INTEGER(5) NOT NULL,
   attitude VARCHAR(45) NOT NULL,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE department (
+  departmentID INT NOT NULL AUTO_INCREMENT,
+  charName VARCHAR(45) NOT NULL,
+  coolness INTEGER(5) NOT NULL,
+  attitude VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+);
+
+
 
 INSERT INTO cast (charName, coolness, attitude)
 VALUES ("Jake Peralta", 90, "Careless");
