fb09cd2e50c043a7d5ee75d08ed15dd345ec1548
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-01-28 21:17:30 -0800

    Create department table in Company_DB.

--- tracker.sql
+++ tracker.sql
@@ -6,30 +6,28 @@ USE Company_DB;
 
 CREATE TABLE employee (
   employeeID INT NOT NULL AUTO_INCREMENT,
-  firstName VARCHAR(45) NOT NULL,
-  lastName VARCHAR(45) NOT NULL,
-  roleID INT(10) NOT NULL,
-  managerID INT(10) NULL 
-  PRIMARY KEY (employeeID)
+  firstName VARCHAR NOT NULL,
+  lastName VARCHAR NOT NULL,
+  roleID INT NOT NULL,
+  managerID INT NULL, 
+  PRIMARY KEY (employeeID),
   FOREIGN KEY (roleID) REFERENCES roles(roleID)
   -- FOREIGN KEY (manager_id) REFERENCES manager(manager_id)
 );
 
 CREATE TABLE roles (
   roleID INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(45) NOT NULL,
-  salary INT(10) NOT NULL,
-  departmentID INT(10) NOT NULL,
-  PRIMARY KEY (roleID)
+  title VARCHAR NOT NULL,
+  salary INT NOT NULL,
+  departmentID INT NOT NULL,
+  PRIMARY KEY (roleID),
   FOREIGN KEY (departmentID) REFERENCES department(departmentID)
 );
 
 CREATE TABLE department (
   departmentID INT NOT NULL AUTO_INCREMENT,
-  charName VARCHAR(45) NOT NULL,
-  coolness INTEGER(5) NOT NULL,
-  attitude VARCHAR(45) NOT NULL,
-  PRIMARY KEY (id)
+  departmentName VARCHAR NOT NULL,
+  PRIMARY KEY (departmentID)
 );
 
 
