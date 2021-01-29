b7b1797ce5b144f467401daee9372aa85e74c037
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-01-28 23:28:18 -0800

    Add add.js, update.js, and view.js queries in queries folder to repo.

---
+++ Assets/Queries/add.js
@@ -0,0 +1,5 @@
+var inquirer = require("inquirer");
+var cTable = require("console.table");
+const Department = require("./Assets/Classes/department");
+const Employee = require("./Assets/Classes/employee");
+const Role = require("./Assets/Classes/role");
